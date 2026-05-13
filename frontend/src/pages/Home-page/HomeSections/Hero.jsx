a18b7039fd266f59e057a8e75c655507826b26d3
Anshuman Panigrahi <panigrahianshuman232@gmail.com>
2026-05-13 19:51:10 +0530

    HeroSection

--- frontend/src/pages/Home-page/HomeSections/Hero.jsx
+++ frontend/src/pages/Home-page/HomeSections/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sky from "../../../assets/imgs/Sky.png"
+
 
 const Hero = () => {
   return (
-    <div>
-      <h1> I am the best</h1>
+    <div className='w-screen h-screen bg-[Sky] bg-cover bg-center bg-no-repeat relative'>
+        <div className='absolute inset-0 md:hidden bg-cover bg-center'></div>
+
     </div>
   )
 }
